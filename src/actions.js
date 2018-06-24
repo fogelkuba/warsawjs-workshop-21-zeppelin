b3c943d77f5ed7f40f567f0af72affdadc27a46d
fogelkuba <fogelkuba@gmail.com>
2018-06-24 17:02:57 +0200

    actions completed

--- src/actions.js
+++ src/actions.js
@@ -26,3 +26,55 @@ export function login({ username, password}){
 }
 
 export const logout = createAction(actionTypes.LOGOUT);
+
+const startReadProjectList = createAction(actionTypes.READ_PROJECT_LIST_START);
+const endReadProjectList = createAction(actionTypes.READ_PROJECT_LIST_END);
+
+export function readProjectList() {
+    return (dispatch, getState) => {
+        dispatch(startReadProjectList());
+        return api
+            .readPostList()
+            .then((response) => {
+                dispatch(endReadProjectList(response.posts));
+            })
+            .catch((error) => {
+                dispatch(endReadProjectList(new Error('Network error')));
+            });
+    };
+}
+
+const startReadProject = createAction(actionTypes.READ_PROJECT_START);
+const endReadProject = createAction(actionTypes.READ_PROJECT_END);
+
+export function readProject(projectId) {
+    return (dispatch, getState) => {
+        dispatch(startReadProject());
+        return api
+            .readPost(projectId)
+            .then((response) => {
+                dispatch(endReadProject({ ...response.post, id: projectId }));
+            })
+            .catch((error) => {
+                dispatch(endReadProject(new Error('Network error')));
+            })
+    };
+}
+
+const startCreateProject = createAction(actionTypes.CREATE_PROJECT_START);
+const endCreateProject = createAction(actionTypes.CREATE_PROJECT_END);
+
+export function createProject({ title, image }) {
+    return (dispatch, getState) => {
+        dispatch(startCreateProject());
+        const username = getUserName(getState());
+        return api
+            .createPost({ username, title, image })
+            .then((response) => {
+                dispatch(endCreateProject(response));
+            })
+            .catch((error) => {
+                dispatch(endCreateProject(new Error('Network error')));
+            });
+    };
+}
