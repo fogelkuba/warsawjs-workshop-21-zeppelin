0a8b8d4cf100ee61a7c825a277a20d8c8527afa0
fogelkuba <fogelkuba@gmail.com>
2018-06-24 12:58:44 +0200

    heder

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,16 +1,50 @@
-import { React, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, TextField, Button } from '@material-ui/core';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+
 import * as urls from '../urls';
-import App from "../App";
+
+const styles = {
+    self: {
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: 300
+    }
+};
 
 export class LoginPage extends PureComponent {
 
-    render(){
-        return(
-            <div>
-                <input type="text"/>
-                <input type="password"/>
-            </div>
-        )
+    state = {
+        submitting: false,
+        username: '',
+        password: '',
+    };
+
+    render() {
+        // const { classes } = this.props;
+        const { username, password } = this.state;
+        return (
+            <form>
+                {/*<div className={classes.self}>*/}
+                <div>
+                    <TextField
+                        label="Username"
+                        name="username"
+                        value={username}
+                        onChange={this.handleChangeUsername}
+                    />
+                    <TextField
+                        label="Password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleChangePassword}
+                    />
+                    {/*<Button variant="raised" onClick={this.handleSubmit}>Login</Button>*/}
+                </div>
+            </form>
+        );
     }
 
 }
